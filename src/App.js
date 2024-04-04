db3eec1f00750e397205805338dc8056499ed7af
Mizanur Rahman <nickty.86@gmail.com>
2024-04-04 23:40:37 +0600

    created route with react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
+
 function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <h1>Webcome to subscription app</h1>
-        <p>lorem</p>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
