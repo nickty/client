9c972beebe766912acc64589e837e937577c9e35
Mizanur Rahman <nickty.86@gmail.com>
2024-04-05 05:23:52 +0600

    custom input

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Input from '../components/Input'
 
 const Register = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <div className="d-flex justify-content-center" style={{ height: '88vh' }}>
       <div className="container align-items-center d-flex">
@@ -9,6 +14,21 @@ const Register = () => {
           <p className="lead pb-4">
             Sign up for free, no credit card required.
           </p>
+          <div className="form-group">
+            <Input label="name" value={name} setValue={setName} />
+            <Input
+              label="email"
+              type="email"
+              value={email}
+              setValue={setEmail}
+            />
+            <Input
+              label="password"
+              type="password"
+              value={password}
+              setValue={setPassword}
+            />
+          </div>
         </div>
       </div>
     </div>
