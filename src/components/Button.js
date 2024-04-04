e396e9ea8524b55e830e1aec0b435c22fcb86f8b
Mizanur Rahman <nickty.86@gmail.com>
2024-04-05 05:36:01 +0600

    design submit button

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Button = ({
+  type = 'primary',
+  size = 'md',
+  text = 'Submit',
+  handleClick,
+}) => {
+  return (
+    <button onClick={handleClick} className={`btn btn-${type} btn-${size}`}>
+      {text}
+    </button>
+  )
+}
+
+export default Button
