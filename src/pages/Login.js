0cf76f1ac1981defa7366719485ff0d72a3204bc
Mizanur Rahman <nickty.86@gmail.com>
2024-04-05 22:11:22 +0600

    redirected to home page after login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Input from '../components/Input'
+import Button from '../components/Button'
+import toast from 'react-hot-toast'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigate = useNavigate()
+
+  const handleClick = async (e) => {
+    try {
+      e.preventDefault()
+      const { data } = await axios.post('http://localhost:8000/api/login', {
+        email,
+        password,
+      })
+
+      if (data.error) {
+        toast.error(data.error)
+      } else {
+        setEmail('')
+        setPassword('')
+
+        navigate('/')
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error('something went wrong')
+    }
+  }
+
   return (
     <div className="d-flex justify-content-center" style={{ height: '88vh' }}>
       <div className="container align-items-center d-flex">
         <div className="row col-md-6 offset-md-3 text-center">
           <h1 className="pt-5 fw-bold">Login</h1>
-          <p className="lead pb-4">
-            Sign up for free, no credit card required.
-          </p>
+          <p className="lead pb-4">Access your subscription</p>
+          <div className="form-group">
+            <Input
+              label="email"
+              type="email"
+              value={email}
+              setValue={setEmail}
+            />
+            <Input
+              label="password"
+              type="password"
+              value={password}
+              setValue={setPassword}
+            />
+
+            <div className="d-grid">
+              <Button handleClick={handleClick} text="Login" size="small" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
